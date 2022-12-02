2fab6e69d7b14e64de741ac1ffb65d69f328f8c1
loicmaes <97891718+loicmaes@users.noreply.github.com>
2022-12-02 01:09:23 +0100

    :lipstick: Base UI elements (html side)

--- src/App.vue
+++ src/App.vue
@@ -3,4 +3,12 @@
 </template>
 
 <style lang="scss">
+@use "./assets/css/schemes/dark";
+@forward "./assets/css/defaults/normalize";
+
+#app {
+  width: 100%;
+  color: dark.$text;
+  min-height: 100vh;
+}
 </style>
